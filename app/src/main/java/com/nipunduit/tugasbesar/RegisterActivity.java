050addc93a310dae91c6e4b2ed611d0191823fa2
zerahya <38621200+zerahya@users.noreply.github.com>
2018-09-12 20:18:38 +0700

    register_complete

--- app/src/main/java/com/nipunduit/tugasbesar/RegisterActivity.java
+++ app/src/main/java/com/nipunduit/tugasbesar/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.nipunduit.tugasbesar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,9 +15,27 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mConfPassword;
     private EditText mTelp;
 
+    private Button mBatal;
+    private Button mDaftar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mName=(EditText)findViewById(R.id.mUsername);
+        mPassword=(EditText)findViewById(R.id.mPassword);
+        mEmail=(EditText) findViewById(R.id.mEmail);
+        mConfPassword=(EditText) findViewById(R.id.mConfPassword);
+        mTelp=(EditText) findViewById(R.id.mTelp);
+        mBatal=(Button)findViewById(R.id.mBatal);
+        mDaftar=(Button)findViewById(R.id.mDaftar);
+
+        mBatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(RegisterActivity.this, MainActivity.class);
+            }
+        });
     }
 }
