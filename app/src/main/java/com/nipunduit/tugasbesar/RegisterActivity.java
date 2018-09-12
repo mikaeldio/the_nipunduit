26d2c8ca46bb32542cacbf4c10ab9017d296a494
zerahya <38621200+zerahya@users.noreply.github.com>
2018-09-12 20:09:35 +0700

    register_ganti

--- app/src/main/java/com/nipunduit/tugasbesar/RegisterActivity.java
+++ app/src/main/java/com/nipunduit/tugasbesar/RegisterActivity.java
@@ -2,8 +2,15 @@ package com.nipunduit.tugasbesar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mConfPassword;
+    private EditText mTelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
