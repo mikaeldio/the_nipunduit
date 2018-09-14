9b3d6573e839dd37450a7b203176733a487515dc
zerahya <38621200+zerahya@users.noreply.github.com>
2018-09-14 12:27:35 +0700

    info keuangan

--- app/src/main/java/com/nipunduit/tugasbesar/InfoKeuangan.java
+++ app/src/main/java/com/nipunduit/tugasbesar/InfoKeuangan.java
@@ -1,13 +1,44 @@
 package com.nipunduit.tugasbesar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InfoKeuangan extends AppCompatActivity {
+
+    private EditText mPendapatan;
+    private EditText mTargetTabungan;
+    private Button mBatal;
+    private Button mDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_keuangan);
+
+        mPendapatan=(EditText)findViewById(R.id.mPendapatan);
+        mTargetTabungan=(EditText)findViewById(R.id.mTarget);
+        mBatal=(Button) findViewById(R.id.mBatal);
+        mDone=(Button) findViewById(R.id.mDone);
+
+        mBatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (InfoKeuangan.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+        mDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (InfoKeuangan.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
