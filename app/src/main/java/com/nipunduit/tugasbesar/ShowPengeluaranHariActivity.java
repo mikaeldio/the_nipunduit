2f01056df54af8854a75a1679f91516621a11e63
Mikael Dio K <mikzpong@gmail.com>
2018-10-03 15:45:28 +0700

    Revert "rewind"
    This reverts commit 38ced5f75a6dfa21549d5a33a8b46820a9c72dd3.

--- app/src/main/java/com/nipunduit/tugasbesar/ShowPengeluaranHariActivity.java
+++ app/src/main/java/com/nipunduit/tugasbesar/ShowPengeluaranHariActivity.java
@@ -1,99 +1,138 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: braces fieldsfirst space lnc 
+
 package com.nipunduit.tugasbesar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ShowPengeluaranHariActivity extends AppCompatActivity {
-    private List<PengeluaranDAO> mListPengeluaranHarian = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private RecycleAdapterHari recycleAdapterHari;
-    private RecyclerView.LayoutManager layoutManager;
-    private TextView mTotal,mDate;
-    private Integer totalPrice = 0;
-    private String nEmail;
+// Referenced classes of package com.nipunduit.tugasbesar:
+//            RecycleAdapterHari, ApiClient, PengeluaranDAO
 
+public class ShowPengeluaranHariActivity extends AppCompatActivity
+{
+
+    private android.support.v7.widget.RecyclerView.LayoutManager layoutManager;
+    private TextView mDate;
+    private List mListPengeluaranHarian;
+    private TextView mTotal;
     private Bundle nBundle;
+    private String nEmail;
+    private String nTanggal;
+    private RecycleAdapterHari recycleAdapterHari;
+    private RecyclerView recyclerView;
+    private Integer totalPrice;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_pengeluaran_hari);
-
-        nBundle=getIntent().getBundleExtra("login");
-        nEmail= nBundle.getString("email");
-
-        //===> Date taker
-        Date c = Calendar.getInstance().getTime();
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        String formattedDate = df.format(c);
-        //<===
-
-        mTotal=(TextView) findViewById(R.id.mTotal);
-
-        recyclerView = findViewById(R.id.recycler_view_hari);
-        mDate =(TextView) findViewById(R.id.mDate);
-        recycleAdapterHari = new RecycleAdapterHari(this, mListPengeluaranHarian);
-        layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(recycleAdapterHari);
-        setRecycleView();
-
-        for(int i=0; i<mListPengeluaranHarian.size(); i++)
-        {
-            totalPrice +=  mListPengeluaranHarian.get(i).getJumlah();
-        }
-        String t = totalPrice.toString();
-        mTotal.setText(t);
-        mDate.setText(formattedDate);
-
+    public ShowPengeluaranHariActivity()
+    {
+        mListPengeluaranHarian = new ArrayList();
+        totalPrice = Integer.valueOf(0);
     }
-    private void setRecycleView(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-        Retrofit.Builder builder=new Retrofit
-                .Builder()
-                .baseUrl("https://nipunduit.000webhostapp.com/api/")
-                .addConverterFactory(GsonConverterFactory.create(gson));
-        Retrofit retrofit=builder.build();
-        ApiClient apiClient=retrofit.create(ApiClient.class);
-        Call<PengeluaranDAO.Value> studentDAOCall=apiClient.getPengeluaranHari(nEmail);
 
-        studentDAOCall.enqueue(new Callback<PengeluaranDAO.Value>() {
-            @Override
-            public void onResponse(Call<PengeluaranDAO.Value> call, Response<PengeluaranDAO.Value> response) {
-                Toast.makeText(ShowPengeluaranHariActivity.this,"Done",Toast.LENGTH_SHORT).show();
-                recycleAdapterHari.notifyDataSetChanged();
-                recycleAdapterHari=new RecycleAdapterHari(ShowPengeluaranHariActivity.this, response.body().getResult());
-                recyclerView.setAdapter(recycleAdapterHari);
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+        setContentView(0x7f0c0024);
+        nBundle = getIntent().getBundleExtra("login");
+        nEmail = nBundle.getString("email");
+        bundle = Calendar.getInstance().getTime();
+        (new SimpleDateFormat("dd-MMM-yyyy")).format(bundle);
+        mTotal = (TextView)findViewById(0x7f090085);
+        mDate = (TextView)findViewById(0x7f09006c);
+        mDate.setText(nTanggal);
+        mListPengeluaranHarian = new ArrayList();
+        recyclerView = (RecyclerView)findViewById(0x7f0900a7);
+        bundle = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(bundle);
+        recycleAdapterHari = new RecycleAdapterHari(getApplicationContext(), mListPengeluaranHarian);
+        recyclerView.setAdapter(recycleAdapterHari);
+        bundle = (new GsonBuilder()).setLenient().create();
+        ((ApiClient)(new retrofit2.Retrofit.Builder()).baseUrl("https://nipunduit.000webhostapp.com/api/").addConverterFactory(GsonConverterFactory.create(bundle)).build().create(com/nipunduit/tugasbesar/ApiClient)).getPengeluaranHari(nEmail).enqueue(new Callback() {
+
+            final ShowPengeluaranHariActivity this$0;
+
+            public void onFailure(Call call, Throwable throwable)
+            {
+                Toast.makeText(ShowPengeluaranHariActivity.this, "Network Connection Error", 0).show();
+                Toast.makeText(ShowPengeluaranHariActivity.this, throwable.toString(), 0).show();
+                throwable.printStackTrace();
             }
 
-            @Override
-            public void onFailure(Call<PengeluaranDAO.Value> call, Throwable t) {
-                Toast.makeText(ShowPengeluaranHariActivity.this,"Network Connection Error",Toast.LENGTH_SHORT).show();
-                Toast.makeText(ShowPengeluaranHariActivity.this,t.toString(),Toast.LENGTH_SHORT).show();
-                t.printStackTrace();
+            public void onResponse(Call call, Response response)
+            {
+                call = ShowPengeluaranHariActivity.this;
+                int j = 0;
+                Toast.makeText(call, "Done", 0).show();
+                mListPengeluaranHarian = (List)response.body();
+                call = new StringBuilder();
+                call.append("Response = ");
+                call.append(mListPengeluaranHarian.toString());
+                Log.d("TAG", call.toString());
+                recycleAdapterHari.setPengeluaranlist(mListPengeluaranHarian);
+                recycleAdapterHari.notifyDataSetChanged();
+                for (; j < mListPengeluaranHarian.size(); j++)
+                {
+                    totalPrice = Integer.valueOf(totalPrice.intValue() + ((PengeluaranDAO)mListPengeluaranHarian.get(j)).getJumlah().intValue());
+                }
+
+                call = totalPrice.toString();
+                mTotal.setText(call);
+            }
+
+            
+            {
+                this$0 = ShowPengeluaranHariActivity.this;
+                super();
             }
         });
+        for (int i = 0; i < mListPengeluaranHarian.size(); i++)
+        {
+            totalPrice = Integer.valueOf(totalPrice.intValue() + ((PengeluaranDAO)mListPengeluaranHarian.get(i)).getJumlah().intValue());
+        }
+
+        bundle = totalPrice.toString();
+        mTotal.setText(bundle);
+        mDate.setText(nTanggal);
     }
+
+
+
+/*
+    static List access$002(ShowPengeluaranHariActivity showpengeluaranhariactivity, List list)
+    {
+        showpengeluaranhariactivity.mListPengeluaranHarian = list;
+        return list;
+    }
+
+*/
+
+
+
+
+/*
+    static Integer access$202(ShowPengeluaranHariActivity showpengeluaranhariactivity, Integer integer)
+    {
+        showpengeluaranhariactivity.totalPrice = integer;
+        return integer;
+    }
+
+*/
+
 }
